4ad8b0ca4a87e074c05d6116f528161d809bc8d8
Eduardo Wolf <eduardogw06@gmail.com>
2022-05-13 10:56:54 -0300

    Form validation

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
 use Illuminate\Http\Request;
 
@@ -22,7 +23,7 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
         $serie = Serie::create($request->all());
         $request->session()->flash('mensagem', "Série com o ID {$serie->id} criada: {$serie->nome}");
