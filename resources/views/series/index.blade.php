c977476bee24a82facce022a66b8c3e65912f576
Eduardo Wolf <eduardogw06@gmail.com>
2022-05-13 10:41:54 -0300

    Improving layout

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -9,16 +9,18 @@
     <div class="alert alert-success">{{$mensagem}}</div>
     @endif
 
-    <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
+    <a href="{{ route('form_criar_serie')  }}" class="btn btn-dark mb-2">Adicionar</a>
 
     <ul class="list-group">
         @foreach($series as $serie)
-            <li class="list-group-item">
+            <li class="list-group-item d-flex justify-content-between btn-sm">
                 {{ $serie->nome }}
                 <form method="post" action="/series/{{ $serie->id }}" onsubmit="return confirm('Tem certeza?')">
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-danger">Excluir</button>
+                    <button class="btn btn-danger">
+                        <i class="far fa-trash-alt"></i>
+                    </button>
                 </form>
             </li>
         @endforeach
